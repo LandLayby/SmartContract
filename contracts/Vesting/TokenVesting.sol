d51685258d18ac7c6efeeed5b2d27b84ff0f817f
Vijayta PC <vijaytab@winjit.com>
2018-08-22 10:23:14 +0530

    modified vesting contract to only work for harambee tokens

--- contracts/Vesting/TokenVesting.sol
+++ contracts/Vesting/TokenVesting.sol
@@ -7,99 +7,63 @@ import "../Math/SafeMath.sol";
 
 /**
  * @title TokenVesting
- * @dev A token holder contract that can release its token balance vesting scheme
+ * owner.
  */
 contract TokenVesting is Owned {
   using SafeMath for uint256;
-  
+ 
   event Released(uint256 amount);
-  event Revoked();
 
   // beneficiary of tokens after they are released
   address public beneficiary;
-  
   uint256 public start;
   uint256 public duration;
+  uint256 public end;
+  HarambeeToken public vestedtoken;
+  uint256 public amountvested;
 
-  bool public revocable;
 
-  mapping (address => uint256) public released;
-  mapping (address => bool) public revoked;
 
   /**
-   * @dev Creates a vesting contract that vests its balance of any HarambeeToken to the
+   * @dev Creates a vesting contract that vests its balance of Harambee token to the
    * _beneficiary, gradually in a linear fashion until _start + _duration. By then all
    * of the balance will have vested.
-   * @param _beneficiary address of the beneficiary to whom vested tokens are transferred   
-   * @param _duration duration in seconds of the period in which the tokens will vest
-   * @param _revocable whether the vesting is revocable or not
    */
-  function ApplyTokenVesting(
+  constructor(
     address _beneficiary,
-    uint256 _start,    
-    uint256 _duration,
-    bool _revocable
+    uint256 _start,
+    uint256 _duration,   
+    HarambeeToken _vestedtoken,
+    uint256 _amounttobevested
   )
     public
   {
-    require(_beneficiary != address(0));    
+    require(_beneficiary != address(0));
     beneficiary = _beneficiary;
-    revocable = _revocable;
-    duration = _duration;    
+    duration = _duration;
     start = _start;
+    end = _start + _duration;
+    vestedtoken = _vestedtoken;
+    amountvested = _amounttobevested;
   }
 
   /**
    * @notice Transfers vested tokens to beneficiary.
-   * @param token HarambeeToken  which is being vested
    */
-  function release(HarambeeToken token) public {
-    uint256 unreleased = releasableAmount(token);
+  function release() public {  
+    require(now >= duration);
+    require(msg.sender == beneficiary);
+    uint256 unreleased = vestedAmount();
     require(unreleased > 0);
-    released[token] = released[token].add(unreleased);
-    token.Transfer(beneficiary, unreleased);
-    
+    amountvested =  0;
+    vestedtoken.transfer(beneficiary, unreleased);
     emit Released(unreleased);
   }
 
   /**
-   * @notice Allows the owner to revoke the vesting. Tokens already vested
-   * remain in the contract, the rest are returned to the owner.
-   * @param token HarambeeToken which is being vested
+   * @dev returns the amount that has  vested.
    */
-  function revoke(HarambeeToken token) public onlyOwner {
-    require(revocable);
-    require(!revoked[token]);
-    uint256 balance = token.balanceOf(this);
-
-    uint256 unreleased = releasableAmount(token);
-    uint256 refund = balance.sub(unreleased);
-
-    revoked[token] = true;
-    token.safeTransfer(owner, refund);
-    emit Revoked();
-  }
-
-  /**
-   * @dev Calculates the amount that has already vested but hasn't been released yet.
-   * @param token HarambeeToken which is being vested
-   */
-  function releasableAmount(HarambeeToken token) public view returns (uint256) {
-    return vestedAmount(token).sub(released[token]);
-  }
-
-  /**
-   * @dev Calculates the amount that has already vested.
-   * @param token HarambeeToken which is being vested
-   */
-  function vestedAmount(HarambeeToken token) public view returns (uint256) {
-    uint256 currentBalance = token.balanceOf(this);
-    uint256 totalBalance = currentBalance.add(released[token]);
-
-    if (block.timestamp >= start.add(duration) || revoked[token]) {
-      return totalBalance;
-    } else {
-      return totalBalance.mul(block.timestamp.sub(start)).div(duration);
-    }
+  function vestedAmount() public view returns (uint256) {
+    return amountvested;
   }
 }
